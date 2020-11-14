39886cb55e59808667540e39f289a2d7b575e140
HCaetano <hrcgeo@gmail.com>
2020-11-14 16:47:54 -0300

    feat: adiciona lógica para renderizar informações básicas do jogador em Feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { gravatarAPI } from '../servicesAPI';
 
 class Feedback extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      score: -1,
+    };
+
+    this.getScoreFromStorage = this.getScoreFromStorage.bind(this);
+  }
+
+  componentDidMount() {
+    this.getScoreFromStorage();
+  }
+
+  getScoreFromStorage() {
+    const { score } = JSON.parse(localStorage.getItem('state')).player;
+    this.setState({ score });
+  }
+
   render() {
+    const { name, gravatarEmail } = this.props;
+    const { score } = this.state;
+
     return (
-      <div>
+      <header>
         <h1 data-testid="feedback-text">Feedback Do Jogo</h1>
-      </div>
+        <p data-testid="header-player-name">{ name }</p>
+        <img
+          alt="user avatar"
+          data-testid="header-profile-picture"
+          src={ gravatarAPI(gravatarEmail) }
+        />
+        <p data-testid="header-score">{ score }</p>
+      </header>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  name: state.player.name,
+});
+
+Feedback.propTypes = {
+  name: PropTypes.string.isRequired,
+  gravatarEmail: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(Feedback);
