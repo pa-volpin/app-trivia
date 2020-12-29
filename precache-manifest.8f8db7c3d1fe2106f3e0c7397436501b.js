1bf9a7c318bd6e67011105e91b6d539c0dfd3bab
pa-volpin <paulo.volpin@gmail.com>
2020-12-29 16:27:57 -0300

    Updates

---
+++ precache-manifest.8f8db7c3d1fe2106f3e0c7397436501b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c11ad26f8043379cd36db0ceaba7a5",
+    "url": "/app-trivia/index.html"
+  },
+  {
+    "revision": "64f653a745f95f2bf19f",
+    "url": "/app-trivia/static/css/main.f5f7f822.chunk.css"
+  },
+  {
+    "revision": "18cef3d16ecc89deaa3b",
+    "url": "/app-trivia/static/js/2.13d2c194.chunk.js"
+  },
+  {
+    "revision": "5d1b8067085602e259fd0862f57df793",
+    "url": "/app-trivia/static/js/2.13d2c194.chunk.js.LICENSE"
+  },
+  {
+    "revision": "64f653a745f95f2bf19f",
+    "url": "/app-trivia/static/js/main.31f8c671.chunk.js"
+  },
+  {
+    "revision": "93295691bb7421ea4d65",
+    "url": "/app-trivia/static/js/runtime-main.1e1f8fb7.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/app-trivia/static/media/trivia.466d153e.png"
+  }
+]);
